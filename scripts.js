d26dddbb368c92ecc23d0fabb746dadaa14161f5
KC Katalbas <kaitlyn.celine@gmail.com>
2020-07-28 10:12:18 -0500

    added score checking function and icons for hands

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 const playerSelection = "rock";
 const computerSelection = computerPlay();
+
+let player = "";
+let comp = "";
 let winner = "";
 
 const rock = document.querySelector('#rock')
@@ -8,7 +11,14 @@ const scissors = document.querySelector('#scissors')
 
 const win = document.querySelector('.winner')
 const score = document.querySelector('.score')
+const reset = document.querySelector('.reset')
 
+const humanHand = document.querySelector("#humanHand")
+const computerHand = document.querySelector("#computerHand")
+
+let scorePlayer = 0
+let scoreComputer = 0
+let roundsPlayed = 0
 
 function computerPlay(){
     let a = Math.floor(Math.random() * Math.floor(3))
@@ -21,9 +31,9 @@ function computerPlay(){
     }
 }
 
-function playRound(a, b) {
-  let player = a.toLowerCase();
-  let comp = b;
+function playRound(player, comp) {
+   player = player.toLowerCase();
+  
   switch(true) {
       case (player == "scissors" && comp == "paper"):
       case (player == "paper" && comp == "rock"):
@@ -41,20 +51,9 @@ function playRound(a, b) {
       default:
           winner = "Something went wrong"
   }
-
   return win.textContent = "player: " + player + " computer: " + comp + " " + winner  
-    }
+}
 
-rock.addEventListener('click', () => {
-    playRound('rock', computerSelection)
-    checkScore(winner)    
-})
-paper.addEventListener('click', () => {
-    playRound('paper', computerSelection)
-})
-scissors.addEventListener('click', () => {
-    playRound('scissors', computerSelection)
-})
 
 
   function checkScore(win) {
@@ -67,4 +66,43 @@ scissors.addEventListener('click', () => {
         } else if (win == "Draw") {
             score.textContent = "score || player: " + scorePlayer + " computer: " + scoreComputer
         }
+    roundsPlayed += 1
+    console.log(roundsPlayed)
   }
+
+  function showResults(player, comp){
+      switch(true) {
+          case(player == "rock"):
+          humanHand.src = "assets/rock.png"
+      }
+
+  }
+
+
+function resetGame() {
+      if(roundsPlayed == 5){
+          if (scorePlayer > scoreComputer){
+              reset.textContent = "You Win!"
+          } else if (scorePlayer < scorePlayer) {
+              reset.textContent = "You Lose!"
+          }
+      }
+      winner = ""
+      scorePlayer = 0
+      scoreComputer = 0
+  }
+
+  rock.addEventListener('click', () => {
+    playRound('rock', computerPlay())
+    checkScore(winner)
+    showResults()
+    resetGame()
+})
+paper.addEventListener('click', () => {
+    playRound('paper', computerPlay())
+    checkScore(winner)
+})
+scissors.addEventListener('click', () => {
+    playRound('scissors', computerPlay())
+    checkScore(winner)
+})
